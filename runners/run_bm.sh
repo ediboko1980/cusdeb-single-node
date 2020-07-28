5b7d524114fbbb2ad1432f648204bb50e0bd5467
Evgeny Golyshev <eugulixes@gmail.com>
2020-07-28 17:12:22 +0300

    Redirect both stdout and stderr to blackmagic.log

--- runners/run_bm.sh
+++ runners/run_bm.sh
@@ -13,5 +13,5 @@ pushd "${TARGET}/blackmagic"
         --mongodb-port="${MONGO_PORT}" \
         --port="${BM_PORT}" \
         --token-key="${TOKEN_KEY}" \
-        --logging=debug 2>> "${TARGET}"/blackmagic.log
+        --logging=debug &>> "${TARGET}"/blackmagic.log
 popd
